523a5be80a4067c7d62963ac22fb21f8b5a18986
Reena <reenaprajapat2601@gmail.com>
2024-02-04 14:13:45 +0530

    initial commit

---
+++ src/components/GameOver.jsx
@@ -0,0 +1,12 @@
+export default function GameOver({winner, onRestart}){
+    return(
+        <div id="game-over">
+            <h2>GAME OVER</h2>
+            {winner && <p>{winner} won</p>}
+            {!winner && <p>It&apos;s a Draw</p>}
+             <p>
+            <button onClick={onRestart}>Rematch !</button>
+            </p>
+        </div>
+    );
+}
